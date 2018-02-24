d0f414f6b74ac3d55a538235be57f532be05f34b
acharyasagar <acharyasaagar@gmail.com>
2018-02-24 15:01:52 -0800

    Axios request for data created

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import Axios from './services/Api'
+import xmlToJson from './services/xmlToJson'
 
-Axios.get('/TheatreAreas')
-.then(data => console.log(data.data))
+
+const d = new Date()
+let today = d.toLocaleDateString() // get today's date as local string
+let url = `/Schedule/?area=1038&dt=${today}` // append date to the url
+
+Axios.get(url)
+.then((data) => {
+   let xmlStr = data.data // get string response
+   let parser = new DOMParser() // instantiate domparser for parsing string to xml
+   let xmlData = parser.parseFromString(xmlStr, "text/xml") // parse string
+   let jsonData = xmlToJson(xmlData) // convert parsed xml to JSON data
+   let shows = jsonData.schedule.shows.show // Get the shows array
+   shows.forEach((show) => {
+    //    console.log(show)
+   })
+})
